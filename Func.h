93a82ab5b4c50890b627fc415294086c4c0b02b5
BB <buithienbaoo2@gmail.com>
2023-05-05 16:19:03 +0700

    Tao cac struct

---
+++ Func.h
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+struct User
+{
+
+    string UserName;
+    string Password;
+};
+
+struct Student
+{
+    int No;
+    string StudentID;
+    string FirstName;
+    string LastName;
+    string Gender;
+    string Birthday;
+    string SocialID;
+};
+
+struct Semester
+{
+    Course *CourseList;
+};
+
+struct Course
+{
+    Student *StudentList;
+    string CourseID, CourseName, ClassName, TeacherName, DayOfWeek;
+    int NumberOfCredits, MaxNumberStudent, Session;
+};
+
+struct Class
+{
+    Student *StudentList;
+    string ClassName;
+};
